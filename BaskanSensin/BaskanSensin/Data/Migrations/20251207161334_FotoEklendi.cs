5d2cee617f9afb72fca949aeb48c7c275265b8c3
Rabiakayikci <rabiakayikci32@gmail.com>
2025-12-07 19:24:45 +0300

    Modeller oluşturuldu, veritabanı bağlantısı kurularak veritabanı oluşturuldu.

---
+++ BaskanSensin/BaskanSensin/Data/Migrations/20251207161334_FotoEklendi.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaskanSensin.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FotoEklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Yapilar",
+                keyColumn: "Yapid",
+                keyValue: 2,
+                column: "Resim",
+                value: "muhendis.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "Yapilar",
+                keyColumn: "Yapid",
+                keyValue: 3,
+                column: "Resim",
+                value: "adalet.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "Yapilar",
+                keyColumn: "Yapid",
+                keyValue: 4,
+                column: "Resim",
+                value: "sanat.jpeg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Yapilar",
+                keyColumn: "Yapid",
+                keyValue: 2,
+                column: "Resim",
+                value: "garden.png");
+
+            migrationBuilder.UpdateData(
+                table: "Yapilar",
+                keyColumn: "Yapid",
+                keyValue: 3,
+                column: "Resim",
+                value: "library.png");
+
+            migrationBuilder.UpdateData(
+                table: "Yapilar",
+                keyColumn: "Yapid",
+                keyValue: 4,
+                column: "Resim",
+                value: "garden.png");
+        }
+    }
+}
---
+++ BaskanSensin/BaskanSensin/Models/Yetenek.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaskanSensin.Models
+{
+    public class Yetenek
+    {
+        
+            [Key]
+            public int Yetid { get; set; }
+        [Display(Name = "Yetenek Adı")]
+            public string Yetad { get; set; }
+
+        }
+    
+}
