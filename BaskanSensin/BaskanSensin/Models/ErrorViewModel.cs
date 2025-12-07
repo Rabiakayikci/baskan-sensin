fc3efc7791f90f070b2fe3e28578772a4d96c50d
Rabiakayikci <rabiakayikci32@gmail.com>
2025-12-07 13:30:46 +0300

    Proje visual studiodan oluşturuldu.

---
+++ BaskanSensin/BaskanSensin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaskanSensin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
